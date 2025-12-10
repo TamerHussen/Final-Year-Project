6806b8114ee7c50740f1b790eb1b2728f1bb0d3e
Lazy <tamer.hussen@mail.bcu.ac.uk>
2025-12-10 19:54:29 +0000

    crouching for player movemnt
    nade it so corouching makes no sound

--- MyFinalProject/Assets/Scripts/PlayerMovement.cs
+++ MyFinalProject/Assets/Scripts/PlayerMovement.cs
@@ -129,17 +129,19 @@ public class PlayerMovement : MonoBehaviour
 
         previousPosition = transform.position;
 
-        float movementSpeeed = characterController.velocity.magnitude; // footsteps equal sound
 
-        if (movementSpeeed > 0.5f) // walking sound
+        if (!isCrouching)
         {
-            SoundEmitter.Emit(transform.position, movementSpeeed);
+            float movementSpeeed = characterController.velocity.magnitude; // footsteps equal sound
+
+            if (movementSpeeed > 0.5f) // walking sound
+                SoundEmitter.Emit(transform.position, movementSpeeed);
+            
+
+            if (isSprinting && movementSpeeed > 1f) // louder sound when sprinting
+                SoundEmitter.Emit(transform.position, movementSpeeed * 1.5f);
         }
 
-        if (isSprinting && movementSpeeed > 1f) // louder sound when sprinting
-        {
-            SoundEmitter.Emit(transform.position, movementSpeeed * 1.5f);
-        }
 
     }
 
